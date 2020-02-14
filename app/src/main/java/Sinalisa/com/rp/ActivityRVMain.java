286f5c0157165b1d008f1c417310c4519e4c7fd1
jefferson <jefferson_1996Th@hotmail.com>
2020-02-14 00:25:19 -0200

    Initial commit

---
+++ app/src/main/java/Sinalisa/com/rp/ActivityRVMain.java
@@ -0,0 +1,14 @@
+package Sinalisa.com.rp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ActivityRVMain extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_recyclerview_main);
+    }
+}
